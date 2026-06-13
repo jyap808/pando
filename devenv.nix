23441178ea93d0bf9832a2ce6c33b09a76732625
Julian Yap <julianokyap@gmail.com>
2026-06-13 01:38:28 -0700

    Add logo footprint, text labels, and SVG gen script

--- devenv.nix
+++ devenv.nix
@@ -2,6 +2,7 @@
 
 {
   scripts.gen.exec = "cd $DEVENV_ROOT/ergogen && ergogen .";
+  scripts.gensvg.exec = "cd $DEVENV_ROOT/ergogen && ergogen --svg .";
   
   enterShell = ''
     echo "Ergogen is ready."
